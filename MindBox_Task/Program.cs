b20ccd923a0d506e338e153d9641afdc555541be
HILTON <90244970+Hi1t0n@users.noreply.github.com>
2023-09-26 21:53:14 +0300

    Add files via upload

---
+++ MindBox_Task/Program.cs
@@ -0,0 +1,16 @@
+﻿using CalculateAreaLib;
+namespace MindBox_Task
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Circle circle = new Circle(5);
+            Console.WriteLine(circle.GetArea());
+            Triangle triangle = new Triangle(3, 4, 5);
+            Console.WriteLine(triangle.GetArea());
+            Console.WriteLine(triangle.IsRightTriangle());
+        }
+    }
+}
